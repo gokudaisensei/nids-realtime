499015114fc4ba151eeea76d3c2112541321c561
Gokul S Unnikrishnan <87324237+gokudaisensei@users.noreply.github.com>
2023-10-01 13:25:53 +0530

    Fixed issue with sniff function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import platform
 import subprocess
-
+import os
 from flask import Flask, render_template, jsonify
 from nids import packet_capture as pk, config, npcap_install
 
@@ -9,12 +9,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pk.start_capture(output_filename=config.get_packet_location() + "test.pcap")
+    pk.start_capture(output_filename=os.path.join(config.get_packet_location(), "captured_packets.pcap"))
     return jsonify({"message": "packets captured"})
     # return render_template("login.html")
 
 
 if __name__ == '__main__':
     config.get_packet_location()
-    npcap_install.main()
     app.run()
--- nids/packet_capture.py
+++ nids/packet_capture.py
@@ -1,15 +1,40 @@
 import time
-
-from scapy.all import sniff, wrpcap
+import socket
+from scapy.all import sniff, wrpcap, IFACES
 
 
 def packet_capture(packet):
     return packet.summary()
 
 
+def get_wireless_interface():
+    try:
+        # Get the IPv4 address
+        target_ipv4 = socket.gethostbyname(socket.gethostname())
+    except socket.gaierror:
+        # Handle the case where hostname cannot be resolved
+        return None
+
+    # Get the list of network interfaces
+    interfaces = list(IFACES.data.values())
+
+    for iface in interfaces:
+        # Check if the IPv4 address of the interface matches the target
+        if iface.ip == target_ipv4:
+            return iface
+    # If no interface matches, return None
+    return None
+
+
 def start_capture(*, output_filename: str):
+    # Get the wireless interface
+    wireless_iface = get_wireless_interface()
+    if wireless_iface is None:
+        print("Unable to find wireless interface.")
+        return
+
     # Initialize an empty list to store packets
-    packets = sniff(prn=packet_capture, count=10)
+    packets = sniff(iface=wireless_iface, prn=packet_capture, count=10)
 
     # Introduce a delay (e.g., 1 second) to ensure the capture is complete
     time.sleep(1)
